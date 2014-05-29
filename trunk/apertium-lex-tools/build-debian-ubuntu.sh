aca435d916f8270be14968c7108b333ac1d57f88
Tino Didriksen <mail@tinodidriksen.com>
2014-05-29 15:49:21 +0000

    apertium-lex-tools packaging and autopkg scripts
    git-svn-id: https://svn.code.sf.net/p/apertium/svn/branches/packaging@53583 72bbbca6-d526-0410-a7d9-f06f51895060

---
+++ trunk/apertium-lex-tools/build-debian-ubuntu.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+cd /tmp
+rm -fv make-deb-source.pl
+wget https://svn.code.sf.net/p/apertium/svn/branches/packaging/trunk/apertium-lex-tools/make-deb-source.pl -O make-deb-source.pl
+chmod +x *.pl
+./make-deb-source.pl -m 'Apertium Automaton <apertium-packaging@lists.sourceforge.net>' -e 'Apertium Automaton <apertium-packaging@lists.sourceforge.net>'
+
+rm -fv /var/cache/pbuilder/result/*
+
+cd /tmp/autopkg.*
+for DISTRO in wheezy jessie sid precise saucy trusty utopic
+do
+	for ARCH in i386 amd64
+	do
+		echo "Updating $DISTRO for $ARCH"
+		cowbuilder --update --basepath /var/cache/pbuilder/base-$DISTRO-$ARCH.cow/
+		echo "Building $DISTRO for $ARCH"
+		time cowbuilder --build *$DISTRO*.dsc --basepath /var/cache/pbuilder/base-$DISTRO-$ARCH.cow/ 2>&1 | tee /home/apertium/public_html/apt/logs/apertium-lex-tools-$DISTRO-$ARCH.log
+	done
+done
+
+rm -f /home/apertium/public_html/apt/logs/apertium-lex-tools-reprepro.log
+for DISTRO in wheezy jessie sid
+do
+	echo "reprepro $DISTRO" >> /home/apertium/public_html/apt/logs/apertium-lex-tools-reprepro.log
+	reprepro -b /home/apertium/public_html/apt/debian/ includedeb $DISTRO /var/cache/pbuilder/result/*$DISTRO*.deb 2>&1 | tee -a /home/apertium/public_html/apt/logs/apertium-lex-tools-reprepro.log
+done
+
+for DISTRO in precise saucy trusty utopic
+do
+	echo "reprepro $DISTRO" >> /home/apertium/public_html/apt/logs/apertium-lex-tools-reprepro.log
+	reprepro -b /home/apertium/public_html/apt/ubuntu/ includedeb $DISTRO /var/cache/pbuilder/result/*$DISTRO*.deb 2>&1 | tee -a /home/apertium/public_html/apt/logs/apertium-lex-tools-reprepro.log
+done
+
+chown -R apertium:apertium /home/apertium/public_html/apt
