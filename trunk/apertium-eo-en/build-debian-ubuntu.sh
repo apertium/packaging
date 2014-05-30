d8b5608435e6d6705c598138bee1b1e360a5f139
Tino Didriksen <mail@tinodidriksen.com>
2014-05-30 12:42:22 +0000

    apertium-eo-en packaging WIP
    git-svn-id: https://svn.code.sf.net/p/apertium/svn/branches/packaging@53619 72bbbca6-d526-0410-a7d9-f06f51895060

---
+++ trunk/apertium-eo-en/build-debian-ubuntu.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+cd /tmp
+rm -fv make-deb-source.pl
+wget https://svn.code.sf.net/p/apertium/svn/branches/packaging/trunk/apertium-eo-en/make-deb-source.pl -O make-deb-source.pl
+chmod +x *.pl
+./make-deb-source.pl -m 'Apertium Automaton <apertium-packaging@lists.sourceforge.net>' -e 'Apertium Automaton <apertium-packaging@lists.sourceforge.net>'
+
+rm -fv /var/cache/pbuilder/result/*
+mkdir -p /home/apertium/public_html/apt/logs/apertium-eo-en/
+
+cd /tmp/autopkg.*
+echo "Updating trusty for amd64"
+cowbuilder --update --no-cowdancer-update --basepath /var/cache/pbuilder/base-trusty-amd64.cow/
+echo "Building trusty for amd64"
+time cowbuilder --build *.dsc --basepath /var/cache/pbuilder/base-trusty-amd64.cow/ 2>&1 | tee /home/apertium/public_html/apt/logs/apertium-eo-en/trusty-amd64.log
+
+rm -f /home/apertium/public_html/apt/logs/apertium-eo-en/reprepro.log
+echo "reprepro" >> /home/apertium/public_html/apt/logs/apertium-eo-en/reprepro.log
+reprepro -b /home/apertium/public_html/apt/ubuntu/ includedeb data /var/cache/pbuilder/result/*_all.deb 2>&1 | tee -a /home/apertium/public_html/apt/logs/apertium-eo-en/reprepro.log
+
+chown -R apertium:apertium /home/apertium/public_html/apt
