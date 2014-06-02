8b731413d67e4484a0ec6b246f7b6eedb9d050f5
Tino Didriksen <mail@tinodidriksen.com>
2014-06-02 14:42:39 +0000

    Foma packaging WIP
    git-svn-id: https://svn.code.sf.net/p/apertium/svn/branches/packaging@53713 72bbbca6-d526-0410-a7d9-f06f51895060

---
+++ external/foma/make-deb-source.pl
@@ -0,0 +1,97 @@
+#!/usr/bin/env perl
+# -*- mode: cperl; indent-tabs-mode: nil; tab-width: 3; cperl-indent-level: 3; -*-
+use utf8;
+use strict;
+use warnings;
+BEGIN {
+	$| = 1;
+	binmode(STDIN, ':encoding(UTF-8)');
+	binmode(STDOUT, ':encoding(UTF-8)');
+}
+use open qw( :encoding(UTF-8) :std );
+
+use Getopt::Long;
+my %opts = (
+	'm' => 'Tino Didriksen <mail@tinodidriksen.com>',
+	'e' => 'Tino Didriksen <mail@tinodidriksen.com>',
+	'dv' => 1,
+	'fv' => 1,
+);
+GetOptions(
+	'm=s' => \$opts{'m'},
+	'e=s' => \$opts{'e'},
+	'distv=i' => \$opts{'dv'},
+	'flavv=i' => \$opts{'fv'},
+);
+
+my %distros = (
+	'wheezy' => 'debian',
+	'jessie' => 'debian',
+	'sid' => 'debian',
+	'precise' => 'ubuntu',
+	'saucy' => 'ubuntu',
+	'trusty' => 'ubuntu',
+	'utopic' => 'ubuntu',
+);
+
+print `rm -rf /tmp/autopkg.*`;
+print `mkdir -pv /tmp/autopkg.$$`;
+chdir "/tmp/autopkg.$$" or die "Could not change folder: $!\n";
+
+print `svn export http://foma.googlecode.com/svn/trunk/foma/Makefile`;
+my $major = 0;
+my $minor = 0;
+my $patch = 0;
+my $revision = `svn log -q -l 1 http://foma.googlecode.com/svn/trunk/foma/ | egrep -o '^r[0-9]+' | egrep -o '[0-9]+'` + 0;
+{
+	local $/ = undef;
+	open FILE, 'Makefile' or die "Could not open Makefile: $!\n";
+	my $data = <FILE>;
+	my ($version) = ($data =~ m@^VERSION.*?([\d.]+)@s);
+	if ($version =~ m@^(\d+)\.(\d+)$@) {
+		$major = $1;
+		$minor = $2;
+	}
+	elsif ($version =~ m@^(\d+)\.(\d+)\.(\d+)$@) {
+		$major = $1;
+		$minor = $2;
+		$patch = $3;
+	}
+	close FILE;
+}
+
+my $version = "$major.$minor.$patch.$revision";
+my $date = `date -u -R`;
+
+print `svn export http://foma.googlecode.com/svn/trunk/foma/ 'foma-$version'`;
+`find 'foma-$version' | LC_ALL=C sort -r > orig.lst`;
+print `tar -jcvf 'foma_$version.orig.tar.bz2' -T orig.lst`;
+print `svn export https://svn.code.sf.net/p/apertium/svn/branches/packaging/external/foma/debian/ 'foma-$version/debian/'`;
+
+foreach my $distro (keys %distros) {
+	my $chver = $version.'-';
+	if ($distros{$distro} eq 'ubuntu') {
+		$chver .= "0ubuntu";
+	}
+	$chver .= $opts{'dv'}."~".$distro.$opts{'fv'};
+	my $chlog = <<CHLOG;
+foma ($chver) $distro; urgency=low
+
+  * Automatic build - see changelog via: svn log http://foma.googlecode.com/svn/trunk/foma/
+
+ -- $opts{e}  $date
+CHLOG
+
+	`cp -al 'foma-$version' 'foma-$chver'`;
+	unlink "foma-$chver/debian/changelog";
+	open FILE, ">foma-$chver/debian/changelog" or die "Could not write to debian/changelog: $!\n";
+	print FILE $chlog;
+	close FILE;
+	print `dpkg-source '-DMaintainer=$opts{m}' '-DUploaders=$opts{e}' -b 'foma-$chver'`;
+	chdir "foma-$chver";
+	print `dpkg-genchanges -S -sa '-m$opts{m}' '-e$opts{e}' > '../foma_$chver\_source.changes'`;
+	chdir '..';
+	print `debsign 'foma_$chver\_source.changes'`;
+}
+
+chdir "/tmp";
