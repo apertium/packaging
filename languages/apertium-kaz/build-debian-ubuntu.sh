3097930bfbaedde500c4b5a6dee2412dfcd7f015
Tino Didriksen <mail@tinodidriksen.com>
2014-06-05 06:28:27 +0000

    kaz packaging
    git-svn-id: https://svn.code.sf.net/p/apertium/svn/branches/packaging@53832 72bbbca6-d526-0410-a7d9-f06f51895060

---
+++ languages/apertium-kaz/build-debian-ubuntu.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+./make-deb-source.pl -m 'Apertium Automaton <apertium-packaging@lists.sourceforge.net>' -e 'Apertium Automaton <apertium-packaging@lists.sourceforge.net>'
+
+rm -fv /var/cache/pbuilder/result/*
+mkdir -p /home/apertium/public_html/apt/logs/apertium-kaz/
+
+cd /tmp/autopkg.*
+for DISTRO in wheezy jessie sid precise saucy trusty utopic
+do
+	echo "Updating $DISTRO for i386"
+	cowbuilder --update --basepath /var/cache/pbuilder/base-$DISTRO-i386.cow/
+	echo "Building $DISTRO for i386"
+	time cowbuilder --build *$DISTRO*.dsc --basepath /var/cache/pbuilder/base-$DISTRO-i386.cow/ 2>&1 | tee /home/apertium/public_html/apt/logs/apertium-kaz/$DISTRO-i386.log
+done
+
+rm -f /home/apertium/public_html/apt/logs/apertium-kaz/reprepro.log
+for DISTRO in wheezy jessie sid precise saucy trusty utopic
+do
+	echo "reprepro $DISTRO" >> /home/apertium/public_html/apt/logs/apertium-kaz/reprepro.log
+	reprepro -b /home/apertium/public_html/apt/nightly/ includedeb $DISTRO /var/cache/pbuilder/result/*$DISTRO*.deb 2>&1 | tee -a /home/apertium/public_html/apt/logs/apertium-kaz/reprepro.log
+done
+
+chown -R apertium:apertium /home/apertium/public_html/apt
