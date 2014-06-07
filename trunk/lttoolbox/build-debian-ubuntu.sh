c4787e584513749c17c084cd49c2f941481353b3
Tino Didriksen <mail@tinodidriksen.com>
2014-06-07 20:39:13 +0000

    More externals; Some generic fixup
    git-svn-id: https://svn.code.sf.net/p/apertium/svn/branches/packaging@53954 72bbbca6-d526-0410-a7d9-f06f51895060

--- trunk/lttoolbox/build-debian-ubuntu.sh
+++ trunk/lttoolbox/build-debian-ubuntu.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-cd /tmp
-rm -fv make-deb-source.pl
-wget https://svn.code.sf.net/p/apertium/svn/branches/packaging/trunk/lttoolbox/make-deb-source.pl -O make-deb-source.pl
-chmod +x *.pl
+PROGNAME=lttoolbox
+
 ./make-deb-source.pl -m 'Apertium Automaton <apertium-packaging@lists.sourceforge.net>' -e 'Apertium Automaton <apertium-packaging@lists.sourceforge.net>'
 
 rm -fv /var/cache/pbuilder/result/*
-mkdir -p /home/apertium/public_html/apt/logs/lttoolbox/
+mkdir -p /home/apertium/public_html/apt/logs/$PROGNAME/
 
 cd /tmp/autopkg.*
 for DISTRO in wheezy jessie sid precise saucy trusty utopic
@@ -15,17 +13,17 @@ do
 	for ARCH in i386 amd64
 	do
 		echo "Updating $DISTRO for $ARCH"
-		cowbuilder --update --basepath /var/cache/pbuilder/base-$DISTRO-$ARCH.cow/
+		cowbuilder --update --basepath /var/cache/pbuilder/base-$DISTRO-$ARCH.cow/ 2>&1 | tee /home/apertium/public_html/apt/logs/$PROGNAME/$DISTRO-$ARCH.log
 		echo "Building $DISTRO for $ARCH"
-		time cowbuilder --build *$DISTRO*.dsc --basepath /var/cache/pbuilder/base-$DISTRO-$ARCH.cow/ 2>&1 | tee /home/apertium/public_html/apt/logs/lttoolbox/$DISTRO-$ARCH.log
+		time cowbuilder --build *$DISTRO*.dsc --basepath /var/cache/pbuilder/base-$DISTRO-$ARCH.cow/ 2>&1 | tee -a /home/apertium/public_html/apt/logs/$PROGNAME/$DISTRO-$ARCH.log
 	done
 done
 
-rm -f /home/apertium/public_html/apt/logs/lttoolbox/reprepro.log
+rm -f /home/apertium/public_html/apt/logs/$PROGNAME/reprepro.log
 for DISTRO in wheezy jessie sid precise saucy trusty utopic
 do
-	echo "reprepro $DISTRO" >> /home/apertium/public_html/apt/logs/lttoolbox/reprepro.log
-	reprepro -b /home/apertium/public_html/apt/nightly/ includedeb $DISTRO /var/cache/pbuilder/result/*$DISTRO*.deb 2>&1 | tee -a /home/apertium/public_html/apt/logs/lttoolbox/reprepro.log
+	echo "reprepro $DISTRO" >> /home/apertium/public_html/apt/logs/$PROGNAME/reprepro.log
+	reprepro -b /home/apertium/public_html/apt/nightly/ includedeb $DISTRO /var/cache/pbuilder/result/*$DISTRO*.deb 2>&1 | tee -a /home/apertium/public_html/apt/logs/$PROGNAME/reprepro.log
 done
 
 chown -R apertium:apertium /home/apertium/public_html/apt
