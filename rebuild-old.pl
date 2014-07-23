83e8bc8cd129c4209bff33cbfc1279ba769d0d77
Tino Didriksen <mail@tinodidriksen.com>
2014-07-23 15:23:15 +0000

    Check if any dependency was rebuilt and trigger rebuild if so
    git-svn-id: https://svn.code.sf.net/p/apertium/svn/branches/packaging@55917 72bbbca6-d526-0410-a7d9-f06f51895060

--- rebuild-old.pl
+++ rebuild-old.pl
@@ -30,33 +30,67 @@ my $pkgs = ();
    close FILE;
 }
 
+my %rebuilt = ();
+
 foreach my $pkg (@$pkgs) {
    # If a package path is given, only rebuild that package, but force a rebuild of it
    if ($ARGV[0] && $ARGV[0] ne @$pkg[0]) {
       next;
    }
 
-   print "@$pkg[0]: ";
+   print "Package: @$pkg[0]\n";
    my $gv = `./get-version.pl --url '@$pkg[1]' --file '@$pkg[2]' 2>/dev/null`;
    chomp($gv);
    my ($version,$srcdate) = split(/\t/, $gv);
-   print "latest $version is ";
+   print "\tlatest: $version\n";
    my ($pkname) = (@$pkg[0] =~ m@([-\w]+)$@);
    my $first = substr($pkname, 0, 1);
    my $oldversion = `dpkg -I ~apertium/public_html/apt/nightly/pool/main/$first/$pkname/$pkname\_*-0ubuntu*~precise*_a*.deb | grep 'Version:' | head -n 1 | egrep -o '[0-9].*\$'`;
    chomp($oldversion);
+   print "\texisting: $oldversion\n";
    my $gt = `dpkg --compare-versions '$version' gt '$oldversion' && echo 1 || echo 0` + 0;
-   if ($gt || $ARGV[0]) {
-      print "newer than $oldversion - triggering rebuild\n";
-      my $cli = "./make-deb-source.pl -p '@$pkg[0]' -u '@$pkg[1]' -v '$version' -d '$srcdate' -m 'Apertium Automaton <apertium-packaging\@lists.sourceforge.net>' -e 'Apertium Automaton <apertium-packaging\@lists.sourceforge.net>'";
+
+   my $rebuild = 0;
+   if (!$gt) {
+      # Current version is not newer, so check if any dependencies were rebuilt
+      # ToDo: This should really check the actual .deb dependencies and files
+      my $deps = (@$pkg[3]) || ('http://svn.code.sf.net/p/apertium/svn/branches/packaging/'.@$pkg[0]);
+      $deps = `svn cat $deps/debian/control | egrep '^Build-Depends:' | sort | uniq`;
+      $deps =~ s@(Build-)?Depends:\s*@@g;
+      foreach my $dep (split(/[,|\n]/, $deps)) {
+         $dep =~ s@\([^)]+\)@@g;
+         $dep =~ s@\s$@@g;
+         $dep =~ s@^\s@@g;
+         if (defined $rebuilt{$dep}) {
+            $rebuild = 1;
+            print "\tdependency $dep was rebuilt\n";
+            last;
+         }
+      }
+   }
+
+   if ($gt || $rebuild || $ARGV[0]) {
+      my $distv = 1;
+      if (!$gt) {
+         # If the current version is the same as the old version, bump the distro version since this means we're rebuilding due to a newer dependency
+         ($distv) = ($oldversion =~ m@(\d+)~\w+$@);
+         ++$distv;
+      }
+      print "\tdistv: $distv\n";
+
+      my $cli = "./make-deb-source.pl -p '@$pkg[0]' -u '@$pkg[1]' -v '$version' --distv '$distv' -d '$srcdate' -m 'Apertium Automaton <apertium-packaging\@lists.sourceforge.net>' -e 'Apertium Automaton <apertium-packaging\@lists.sourceforge.net>'";
       if (@$pkg[3]) {
          $cli .= " -r '@$pkg[3]'";
       }
-      print "$cli\n";
+      print "\tlaunching: $cli\n";
       `$cli >&2`;
       `./build-debian-ubuntu.sh '$pkname' >&2`;
-   }
-   else {
-      print "same or older than $oldversion\n";
+
+      my $ls = `ls -1 ~apertium/public_html/apt/nightly/pool/main/$first/$pkname/ | egrep -o '^[^_]+' | sort | uniq`;
+      foreach my $pk (split(/\s+/, $ls)) {
+         chomp($pk);
+         $rebuilt{$pk} = 1;
+         print "\trebuilt: $pk\n";
+      }
    }
 }
