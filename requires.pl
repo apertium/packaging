b440bf6a17635fcb24bd707ded10565ff5551760
Tino Didriksen <mail@tinodidriksen.com>
2014-07-24 12:34:31 +0000

    GPLv3 for some; Remove dependencies that apertium will pull in anyway
    git-svn-id: https://svn.code.sf.net/p/apertium/svn/branches/packaging@55964 72bbbca6-d526-0410-a7d9-f06f51895060

---
+++ requires.pl
@@ -0,0 +1,44 @@
+#!/usr/bin/env perl
+# -*- mode: cperl; indent-tabs-mode: nil; tab-width: 3; cperl-indent-level: 3; -*-
+use utf8;
+use strict;
+use warnings;
+BEGIN {
+	$| = 1;
+	binmode(STDIN, ':encoding(UTF-8)');
+	binmode(STDOUT, ':encoding(UTF-8)');
+}
+use open qw( :encoding(UTF-8) :std );
+
+my %build = ();
+my %deps = ();
+
+while (<>) {
+   if (/\bhfst-lecx\b/ || /\bhfst-twolc\b/) {
+      $build{'hfst'} = 1;
+      $deps{'hfst'} = 1;
+   }
+   elsif (/\bcg-comp\b/ || /\bcg-proc\b/ || /\bcg-conv\b/ || /\bvislc3g\b/ || /\bcg3-autobin\b/) {
+      $build{'cg3'} = 1;
+      $deps{'cg3'} = 1;
+   }
+   elsif (/\blrx-comp\b/ || /\blrx-proc\b/) {
+      $build{'apertium-lex-tools'} = 1;
+      $deps{'apertium-lex-tools'} = 1;
+   }
+   elsif (/\bAP_CHECK_LING.*?(apertium-\w+)/) {
+      $build{$1} = 1;
+   }
+}
+
+print "Build-Depends: debhelper (>= 8.0), locales, dh-autoreconf, autotools-dev, apertium (>= 3.3), libapertium3-3.3-dev, pkg-config (>= 0.21)";
+foreach my $k (sort(keys(%build))) {
+   print ", $k";
+}
+print "\n";
+
+print "Depends: apertium (>= 3.3)";
+foreach my $k (sort(keys(%deps))) {
+   print ", $k";
+}
+print ", \${shlibs:Depends}, \${misc:Depends}\n";
