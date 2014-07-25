efa77b55a7026f88afbe994c4c331cccb7565873
Tino Didriksen <mail@tinodidriksen.com>
2014-07-25 16:37:04 +0000

    Detect gawk dependency
    git-svn-id: https://svn.code.sf.net/p/apertium/svn/branches/packaging@56026 72bbbca6-d526-0410-a7d9-f06f51895060

--- depends.pl
+++ depends.pl
@@ -28,6 +28,9 @@ while (<>) {
    elsif (/\bPKG_CHECK_MODULES.*?lttoolbox\b/) {
       $build{'liblttoolbox3-3.3-dev'} = 1;
    }
+   elsif (/\bAC_PROG_AWK\b/) {
+      $build{'gawk'} = 1;
+   }
    elsif (/\bcg-comp\b/ || /\bcg-proc\b/ || /\bcg-conv\b/ || /\bvislc3g\b/ || /\bcg3-autobin\b/) {
       $build{'cg3'} = 1;
       $deps{'cg3'} = 1;
