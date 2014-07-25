4e51c0c615de6472f3065d53f2409cc63e81e911
Tino Didriksen <mail@tinodidriksen.com>
2014-07-25 09:28:05 +0000

    Add comments. Script functional.
    git-svn-id: https://svn.code.sf.net/p/apertium/svn/branches/packaging@56003 72bbbca6-d526-0410-a7d9-f06f51895060

--- rebuild-old.pl
+++ rebuild-old.pl
@@ -36,8 +36,9 @@ my %rebuilt = ();
 my %blames = ();
 
 use IO::Tee;
-open my $log, '>rebuild.log' or die "Failed to open rebuilt.log: $!\n";
-my $out = IO::Tee->new($log, \*STDOUT);
+open my $log, ">/tmp/rebuild.$$.log" or die "Failed to open rebuild.log: $!\n";
+my $out2 = IO::Tee->new($log, \*STDOUT);
+print {$out2} "Build started ".`date -u`;
 
 foreach my $pkg (@$pkgs) {
    # If a package path is given, only rebuild that package, but force a rebuild of it
@@ -45,16 +46,25 @@ foreach my $pkg (@$pkgs) {
       next;
    }
 
+   my ($pkname) = (@$pkg[0] =~ m@([-\w]+)$@);
+   open my $pkglog, ">/tmp/rebuild-$pkname.$$.log" or die "Failed to open rebuild-pkg.log: $!\n";
+   my $out = IO::Tee->new($out2, $pkglog);
+
    if (!@$pkg[2]) {
       @$pkg[2] = 'configure.ac';
    }
 
+   print {$out} "\n";
    print {$out} "Package: @$pkg[0]\n";
+   print {$out} "\tstarted: ".`date -u`;
+
+   # Determine latest version and date stamp from the repository
    my $gv = `./get-version.pl --url '@$pkg[1]' --file '@$pkg[2]' 2>/dev/null`;
    chomp($gv);
    my ($version,$srcdate) = split(/\t/, $gv);
    print {$out} "\tlatest: $version\n";
-   my ($pkname) = (@$pkg[0] =~ m@([-\w]+)$@);
+
+   # Determine existing package version, if any
    my $first = substr($pkname, 0, 1);
    my $oldversion = '0.0.0.0';
    if (-e "/home/apertium/public_html/apt/nightly/pool/main/$first/$pkname") {
@@ -62,12 +72,15 @@ foreach my $pkg (@$pkgs) {
       chomp($oldversion);
    }
    print {$out} "\texisting: $oldversion\n";
+
+   # Figure out if the latest is newer than existing, which is complicated enough that we just ask dpkg
    my $gt = `dpkg --compare-versions '$version' gt '$oldversion' && echo 1 || echo 0` + 0;
 
    my $rebuild = 0;
    if (!$gt) {
       # Current version is not newer, so check if any dependencies were rebuilt
       # ToDo: This should really check the actual .deb dependencies and files
+      # Counter-ToDo: Since .deb packages have no clue about Build-Depends, we'd need to check both anyway, so this is fine for now
       my $deps = (@$pkg[3]) || ('http://svn.code.sf.net/p/apertium/svn/branches/packaging/'.@$pkg[0]);
       $deps = `svn cat $deps/debian/control | egrep '^Build-Depends:' | sort | uniq`;
       $deps =~ s@(Build-)?Depends:\s*@@g;
@@ -83,77 +96,105 @@ foreach my $pkg (@$pkgs) {
       }
    }
 
-   if ($gt || $rebuild || $ARGV[0]) {
-      my $distv = 1;
-      if (!$gt) {
-         # If the current version is the same as the old version, bump the distro version since this means we're rebuilding due to a newer dependency
-         ($distv) = ($oldversion =~ m@(\d+)$@);
-         ++$distv;
-      }
-      print {$out} "\tdistv: $distv\n";
-
-      my $cli = "./make-deb-source.pl -p '@$pkg[0]' -u '@$pkg[1]' -v '$version' --distv '$distv' -d '$srcdate' -m 'Apertium Automaton <apertium-packaging\@lists.sourceforge.net>' -e 'Apertium Automaton <apertium-packaging\@lists.sourceforge.net>'";
-      if (@$pkg[3]) {
-         $cli .= " -r '@$pkg[3]'";
-      }
-      print {$out} "\tlaunching rebuild\n";
-      `$cli >&2`;
-      my $is_data = '';
-      if (@$pkg[0] =~ m@^languages/@ || @$pkg[0] =~ m@/apertium-\w{2,3}-\w{2,3}@) {
-         print {$out} "\tdata only\n";
-         $is_data = 'data';
-      }
-      `./build-debian-ubuntu.sh '$pkname' '$is_data' >&2`;
-
-      my $failed = `grep -L 'dpkg-genchanges' \$(grep -l 'Copying COW directory' \$(find /home/apertium/public_html/apt/logs/$pkname -newermt \$(date '+\%Y-\%m-\%d' -d '1 day ago') -type f))`;
-      chomp($failed);
-      if ($failed) {
-         print {$out} "\tFAILED:\n";
-         foreach my $fail (split(/\n/, $failed)) {
-            chomp($fail);
-            $fail =~ s@^/home/apertium/public_html@http://apertium.projectjj.com@;
-            print {$out} "\t\t$fail\n";
-         }
-         my ($oldrev) = $oldversion =~ m@\.(\d+)$@;
-         ++$oldrev;
-         my ($newrev) = $version =~ m@\.(\d+)$@;
-         my $blames = `svn log -q -r$oldrev:$newrev '@$pkg[1]' | egrep '^r' | awk '{ print \$3 }' | sort | uniq`;
-         chomp($blames);
-         print {$out} "\tblames in revisions $oldrev:$newrev :\n";
-         foreach my $blame (split(/\n/, $blames)) {
-            chomp($blame);
-            $blames{$blame} = 1;
-            print {$out} "\t\t$blame\n";
-         }
-         next;
-      }
-
-      `./reprepro.sh '$pkname' >&2`;
-
-      my $ls = `ls -1 ~apertium/public_html/apt/nightly/pool/main/$first/$pkname/ | egrep -o '^[^_]+' | sort | uniq`;
-      foreach my $pk (split(/\s+/, $ls)) {
-         chomp($pk);
-         $rebuilt{$pk} = 1;
-         print {$out} "\trebuilt: $pk\n";
-      }
+   if (!($gt || $rebuild || $ARGV[0])) {
+      # Package doesn't need rebuilding, so skip to cleanup
+      goto CLEANUP;
    }
+
+   my $distv = 1;
+   if (!$gt) {
+      # If the current version is the same as the old version, bump the distro version since this means we're rebuilding due to a newer dependency
+      ($distv) = ($oldversion =~ m@(\d+)$@);
+      ++$distv;
+   }
+   print {$out} "\tdistv: $distv\n";
+
+   # Create the source packages
+   my $cli = "./make-deb-source.pl -p '@$pkg[0]' -u '@$pkg[1]' -v '$version' --distv '$distv' -d '$srcdate' -m 'Apertium Automaton <apertium-packaging\@lists.sourceforge.net>' -e 'Apertium Automaton <apertium-packaging\@lists.sourceforge.net>'";
+   if (@$pkg[3]) {
+      $cli .= " -r '@$pkg[3]'";
+   }
+   print {$out} "\tlaunching rebuild\n";
+   `$cli >&2`;
+   my $is_data = '';
+   if (@$pkg[0] =~ m@^languages/@ || @$pkg[0] =~ m@/apertium-\w{2,3}-\w{2,3}$@) {
+      # If this is a data-only package, only build it for one arch per distro
+      print {$out} "\tdata only\n";
+      $is_data = 'data';
+   }
+   # Build the packages
+   `./build-debian-ubuntu.sh '$pkname' '$is_data' >&2`;
+   print {$out} "\tstopped: ".`date -u`;
+
+   my $failed = `grep -L 'dpkg-genchanges' \$(grep -l 'Copying COW directory' \$(find /home/apertium/public_html/apt/logs/$pkname -newermt \$(date '+\%Y-\%m-\%d' -d '1 day ago') -type f))`;
+   chomp($failed);
+   if ($failed) {
+      # Gather up URLs for the logs of the failed builds
+      print {$out} "\tFAILED:\n";
+      foreach my $fail (split(/\n/, $failed)) {
+         chomp($fail);
+         $fail =~ s@^/home/apertium/public_html@http://apertium.projectjj.com@;
+         print {$out} "\t\t$fail\n";
+      }
+
+      # Determine who was most likely responsible for breaking the build
+      my ($oldrev) = ($oldversion =~ m@\.(\d+)$@);
+      ++$oldrev;
+      my ($newrev) = ($version =~ m@\.(\d+)$@);
+      my $blames = `svn log -q -r$oldrev:$newrev '@$pkg[1]' | egrep '^r' | awk '{ print \$3 }' | sort | uniq`;
+      chomp($blames);
+      print {$out} "\tblames in revisions $oldrev:$newrev :\n";
+      my $cc = '';
+      foreach my $blame (split(/\n/, $blames)) {
+         chomp($blame);
+         $blames{$blame} = 1;
+         print {$out} "\t\t$blame\n";
+         # Add the suspect to CC so they are directly notified
+         $cc .= " '$blame\@users.sourceforge.net'";
+      }
+
+      my $subject = "[TEST] @$pkg[0] failed nightly build";
+      if (!$ARGV[0]) {
+         `cat /tmp/rebuild-$pkname.$$.log | mail -s '$subject' -r 'apertium-packaging\@projectjj.com' 'apertium-packaging\@lists.sourceforge.net' $cc`;
+      }
+      goto CLEANUP;
+   }
+
+   # Add the resulting .deb to the Apt repository
+   # Note that this does not happen if ANY failure was detected, to ensure we don't get partially-updated trees
+   `./reprepro.sh '$pkname' >&2`;
+
+   # Get a list of resulting packages and mark them all as rebuilt
+   my $ls = `ls -1 ~apertium/public_html/apt/nightly/pool/main/$first/$pkname/ | egrep -o '^[^_]+' | sort | uniq`;
+   foreach my $pk (split(/\s+/, $ls)) {
+      chomp($pk);
+      $rebuilt{$pk} = 1;
+      print {$out} "\trebuilt: $pk\n";
+   }
+
+   CLEANUP:
+   close $pkglog;
+   unlink("/tmp/rebuild-$pkname.$$.log");
 }
 
+print {$out2} "\n";
+print {$out2} "Build stopped at ".`date -u`;
 close $log;
 
+# If any package was (attempted) rebuilt, send a status email
 if (!$ARGV[0] && (%rebuilt || %blames)) {
    my $subject = '[TEST] Nightly';
-   my $cc = '';
    if (%blames) {
       $subject .= ': Failures (att:';
       foreach my $blame (sort(keys(%blames))) {
          $subject .= " $blame";
-         $cc .= " '$blame\@users.sourceforge.net'";
       }
       $subject .= ')';
    }
    else {
       $subject .= ': Success';
    }
-   `cat rebuild.log | mail -s '$subject' -r 'apertium-packaging\@projectjj.com' 'apertium-packaging\@lists.sourceforge.net' $cc`;
+   `cat /tmp/rebuild.$$.log | mail -s '$subject' -r 'apertium-packaging\@projectjj.com' 'apertium-packaging\@lists.sourceforge.net'`;
 }
+
+unlink("/tmp/rebuild.$$.log");
