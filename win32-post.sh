c5dbc46dcda1c5f0c097223994f4d121a4b55876
Tino Didriksen <mail@tinodidriksen.com>
2015-02-24 15:04:34 +0000

    apertium-lex-tools win32
    git-svn-id: https://svn.code.sf.net/p/apertium/svn/branches/packaging@59112 72bbbca6-d526-0410-a7d9-f06f51895060

--- win32-post.sh
+++ win32-post.sh
@@ -7,8 +7,8 @@ for DEP in $EXTRA_DEPS
 do
 	cp -av /opt/mxe/usr/i686-w64-mingw32.shared/bin/$DEP win32/bin/
 done
-find . -type f -name '*.exe' -or -name '*.dll' | xargs -n1 /opt/mxe/usr/bin/i686-w64-mingw32.shared-strip
-find . -type f -name '*.a' | xargs -n1 /opt/mxe/usr/bin/i686-w64-mingw32.shared-strip --strip-debug
+find . -type f -name '*.exe' -or -name '*.dll' | xargs -rn1 /opt/mxe/usr/bin/i686-w64-mingw32.shared-strip
+find . -type f -name '*.a' | xargs -rn1 /opt/mxe/usr/bin/i686-w64-mingw32.shared-strip --strip-debug
 mv win32 $PKG_NAME
 #zip -9r $PKG_NAME-$PKG_VER.zip $PKG_NAME
 7za a $PKG_NAME-$PKG_VER.7z $PKG_NAME
