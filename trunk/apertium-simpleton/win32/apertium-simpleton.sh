1add680794c8988a1d6270e34c49da601e132774
Tino Didriksen <mail@tinodidriksen.com>
2015-06-11 06:32:10 +0000

    Different DLLs
    git-svn-id: https://svn.code.sf.net/p/apertium/svn/branches/packaging@60726 72bbbca6-d526-0410-a7d9-f06f51895060

--- trunk/apertium-simpleton/win32/apertium-simpleton.sh
+++ trunk/apertium-simpleton/win32/apertium-simpleton.sh
@@ -1,8 +1,8 @@
 /opt/mxe/usr/i686-w64-mingw32.shared/qt5/bin/qmake apertium-simpleton.pro PREFIX=/opt/win32
 QT5="../qt5/bin"
-export EXTRA_DEPS="icudt54.dll icuin54.dll icuio54.dll icuuc54.dll libharfbuzz-0.dll libpng16-16.dll libpcre16-0.dll libpcre-1.dll libiconv-2.dll libfreetype-6.dll libglib-2.0-0.dll libbz2.dll libintl-8.dll zlib1.dll $QT5/Qt5Core.dll $QT5/Qt5Gui.dll $QT5/Qt5Widgets.dll $QT5/Qt5Network.dll $EXTRA_DEPS"
+export EXTRA_DEPS="icudt54.dll icuin54.dll icuuc54.dll libharfbuzz-0.dll libpng16-16.dll libpcre16-0.dll libpcre-1.dll libiconv-2.dll libfreetype-6.dll libglib-2.0-0.dll libbz2.dll libintl-8.dll zlib1.dll ssleay32.dll libeay32.dll $QT5/Qt5Core.dll $QT5/Qt5Gui.dll $QT5/Qt5Widgets.dll $QT5/Qt5Network.dll $EXTRA_DEPS"
 export EXTRA_INST="release/apertium-simpleton.exe"
 
 mkdir -pv /opt/win32-pkg/$PKG_NAME/opt/win32/bin
-rsync -av /opt/win32/bin /opt/win32-pkg/$PKG_NAME/opt/win32/
+rsync -av /opt/mxe/usr/i686-w64-mingw32.shared/qt5/plugins/bearer /opt/win32-pkg/$PKG_NAME/opt/win32/bin/
 rsync -av /opt/mxe/usr/i686-w64-mingw32.shared/qt5/plugins/platforms /opt/win32-pkg/$PKG_NAME/opt/win32/bin/
