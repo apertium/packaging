846fdef9edb021971fbbbd962fb39de166a9d7cb
Tino Didriksen <mail@tinodidriksen.com>
2015-06-22 09:46:45 +0000

    HFST SONAME was bumped
    git-svn-id: https://svn.code.sf.net/p/apertium/svn/branches/packaging@61005 72bbbca6-d526-0410-a7d9-f06f51895060

--- depends.pl
+++ depends.pl
@@ -20,7 +20,7 @@ while (<>) {
       next;
    }
    if (/\bPKG_CHECK_MODULES.*?hfst\b/) {
-      $build{'libhfst40-dev'} = 1;
+      $build{'libhfst41-dev'} = 1;
       $build{'hfst'} = 1;
       $deps{'hfst'} = 1;
    }
