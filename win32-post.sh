76b16f1006d2ee860ffc6b8262b11b2f42fd2afb
Tino Didriksen <mail@tinodidriksen.com>
2016-03-21 14:05:23 +0000

    No really, .la files are not welcome
    git-svn-id: https://svn.code.sf.net/p/apertium/svn/branches/packaging@66696 72bbbca6-d526-0410-a7d9-f06f51895060

--- win32-post.sh
+++ win32-post.sh
@@ -19,6 +19,7 @@ set +e
 find . -type f -name '*.exe' -or -name '*.dll' | grep -v 7z | xargs -rn1 /opt/mxe/usr/bin/i686-w64-mingw32.shared-strip
 find . -type f -name '*.exe' -or -name '*.dll' | grep -v 7z | xargs -rn1 chmod uga+x
 find . -type f -name '*.a' | xargs -rn1 /opt/mxe/usr/bin/i686-w64-mingw32.shared-strip --strip-debug
+find . -type f -name '*.la' | xargs -rn1 rm -f 2>/dev/null
 set -e
 chmod -R uga+r win32
 mv win32 $PKG_NAME
