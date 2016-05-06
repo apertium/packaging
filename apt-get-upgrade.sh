e6171c265840431acc180519759b2cfafdcabd54
Tino Didriksen <mail@tinodidriksen.com>
2016-05-06 14:45:03 +0000

    WIP Matxin
    git-svn-id: https://svn.code.sf.net/p/apertium/svn/branches/packaging@68080 72bbbca6-d526-0410-a7d9-f06f51895060

--- apt-get-upgrade.sh
+++ apt-get-upgrade.sh
@@ -3,4 +3,5 @@ set -e
 
 apt-get -qy update -o Dir::Etc::sourcelist="sources.list.d/apertium-nightly.list" -o Dir::Etc::sourceparts="-" -o APT::Get::List-Cleanup="0"
 apt-get -qfy install $(aptitude search '~O apertium' | egrep -o 'apertium-\w+-\w+' | grep -v lex-tools | grep -v all-dev | sort | uniq)
+# apt-get -qfy install $(aptitude search '~O apertium' | grep 'single language' | egrep -o '(apertium|giella)-\w+' | sort | uniq)
 su - apertium -c 'screen -r beta-apy -X quit && nice -n20 screen -dmS beta-apy /home/apertium/apertium-apy-beta/run-apy.sh'
