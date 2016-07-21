621ee96b6e9f3a15a754255485f60e9c4bc329ba
Tino Didriksen <mail@tinodidriksen.com>
2016-07-21 12:34:51 +0000

    MinGW -fno-use-linker-plugin
    git-svn-id: https://svn.code.sf.net/p/apertium/svn/branches/packaging@71126 72bbbca6-d526-0410-a7d9-f06f51895060

--- win32-pre.sh
+++ win32-pre.sh
@@ -11,6 +11,7 @@ export PKG_VER=$3
 export PKG_PATH=$4
 export EXTRA_DEPS="libstdc++-6.dll libgcc_s_sjlj-1.dll"
 export EXTRA_INST=""
+export LDFLAGS="-fno-use-linker-plugin"
 rm -rf /opt/win32
 rm -rf /opt/win32-pkg/$PKG_NAME
 cd /opt/win32-build/$PKG_NAME
