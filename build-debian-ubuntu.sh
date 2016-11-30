5e029bddc85d985cb0229a367c3f413c706d45ff
Tino Didriksen <mail@tinodidriksen.com>
2016-11-30 08:33:11 +0000

    90 minutes instead, 'cause HFST managed to exceed 60 minutes
    git-svn-id: https://svn.code.sf.net/p/apertium/svn/branches/packaging@74515 72bbbca6-d526-0410-a7d9-f06f51895060

--- build-debian-ubuntu.sh
+++ build-debian-ubuntu.sh
@@ -31,7 +31,7 @@ do
 		fi
 
 		echo "Building $DISTRO for $ARCH"
-		cowbuilder --build *$DISTRO*.dsc --basepath /var/cache/pbuilder/base-$DISTRO-$ARCH.cow/ >>/home/apertium/public_html/apt/logs/$1/$DISTRO-$ARCH.log 2>&1 &
+		timeout 90m cowbuilder --build *$DISTRO*.dsc --basepath /var/cache/pbuilder/base-$DISTRO-$ARCH.cow/ >>/home/apertium/public_html/apt/logs/$1/$DISTRO-$ARCH.log 2>&1 &
 		if [[ -n "$2" ]]; then
 			break
 		fi
