99fc01a94ea8e6a5046c97bd08bcafb723882739
Tino Didriksen <mail@tinodidriksen.com>
2016-12-08 12:19:56 +0000

    Package both win32 and win64 spellers
    git-svn-id: https://svn.code.sf.net/p/apertium/svn/branches/packaging@74723 72bbbca6-d526-0410-a7d9-f06f51895060

--- speller-funcs.sh
+++ speller-funcs.sh
@@ -1,17 +1,36 @@
 #!/bin/bash
 
+function pkg_msi {
+	pushd /misc/spellers
+	WINX=$2
+	./make_msi.pl $1
+	mkdir -pv ~apertium/public_html/spellers/${BUILDTYPE}/
+	rm -fv ~apertium/public_html/spellers/${BUILDTYPE}/$1*-$2*
+	cp -av build/$1/*.msi ~apertium/public_html/spellers/${BUILDTYPE}/
+	popd
+	pushd ~apertium/public_html/spellers/${BUILDTYPE}/
+	ln -sfv $1*-$2.msi $1-latest-$2.msi
+	popd
+}
+
+function pkg_xpi {
+	pushd /misc/spellers
+	./make_xpi.pl $1
+	mkdir -pv ~apertium/public_html/spellers/${BUILDTYPE}/
+	rm -fv ~apertium/public_html/spellers/${BUILDTYPE}/$1*.xpi
+	cp -av build/mozilla/$1*.xpi ~apertium/public_html/spellers/${BUILDTYPE}/
+	popd
+	pushd ~apertium/public_html/spellers/${BUILDTYPE}/
+	ln -sfv $1*.xpi $1-latest.xpi
+	popd
+}
+
 function pkg_spellers {
 	pushd /misc/spellers
 	svn up
-	./make_msi.pl $1
-	./make_xpi.pl $1
-	mkdir -pv ~apertium/public_html/spellers/
-	rm -fv ~apertium/public_html/spellers/$1*
-	cp -av build/$1/*.msi build/mozilla/$1*.xpi ~apertium/public_html/spellers/
 	popd
-	pushd ~apertium/public_html/spellers/
-	ln -sfv $1*.msi $1-latest.msi
-	ln -sfv $1*.xpi $1-latest.xpi
-	popd
-	chown -R apertium:apertium ~apertium/public_html/spellers
+	pkg_msi $1 win32
+	pkg_msi $1 win64
+	pkg_xpi $1
+	chown -R apertium:apertium ~apertium/public_html/spellers/${BUILDTYPE}
 }
