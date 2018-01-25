e9e115a3a5a19cc3cd2cf2fcbe535dbd97bb2d30
Tino Didriksen <mail@tinodidriksen.com>
2018-01-25 10:05:52 +0000

    WIP Debian Buster
    git-svn-id: https://svn.code.sf.net/p/apertium/svn/branches/packaging@83886 72bbbca6-d526-0410-a7d9-f06f51895060

---
+++ scripts/apt-install-nightly.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+if [[ `whoami` != root ]]
+then
+	echo "You must run this Apertium install script as root, or via sudo!"
+	exit -1
+fi
+
+CADENCE=nightly
+
+echo "Cleaning up old install, if any..."
+rm -fv /etc/apt/trusted.gpg.d/apertium* /etc/apt/preferences.d/apertium* /etc/apt/sources.list.d/apertium*
+
+echo "Determining Debian/Ubuntu codename..."
+P=`apt-cache policy`
+P="$P "`lsb_release -c`"/dummy"
+P="$P "`grep CODENAME /etc/lsb-release`"/dummy"
+DISTRO=""
+for D in sid jessie stretch buster trusty xenial zesty artful bionic
+do
+	if [[ $P == *$D/* ]]
+	then
+		DISTRO=$D
+		echo "Found evidence of $D..."
+	fi
+done
+if [[ -z "$DISTRO" ]]
+then
+	echo "No supported Debian or Ubuntu derivative detected - bailing out..."
+	exit -1
+fi
+echo "Settling for $DISTRO - enabling the Apertium $CADENCE repo..."
+
+echo "Installing Apertium GnuPG key to /etc/apt/trusted.gpg.d/apertium.gpg"
+wget -q https://apertium.projectjj.com/apt/apertium-packaging.public.gpg -O /etc/apt/trusted.gpg.d/apertium.gpg
+
+echo "Installing package override to /etc/apt/preferences.d/apertium.pref"
+wget -q https://apertium.projectjj.com/apt/apertium.pref -O /etc/apt/preferences.d/apertium.pref
+
+echo "Creating /etc/apt/sources.list.d/apertium.list"
+echo "deb http://apertium.projectjj.com/apt/$CADENCE $DISTRO main" > /etc/apt/sources.list.d/apertium.list
+
+echo "Running apt-get update..."
+apt-get -qy update >/dev/null 2>&1
+
+echo "All done - enjoy the packages! If you just want all core tools, do: sudo apt-get install apertium-all-dev"
---
+++ scripts/rpm-install-nightly.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+if [[ `whoami` != root ]]
+then
+	echo "You must run this Apertium install script as root, or via sudo!"
+	exit -1
+fi
+
+CADENCE=nightly
+
+echo "Determining Fedora/CentOS/RHEL/OpenSUSE version..."
+P=`cat /etc/*-release`
+DISTRO=
+TOOL=yum
+case $P in
+	*Red*Hat*Enterprise*Linux*7*|*Scientific*Linux*7*|*CentOS*7*)
+		DISTRO="CentOS_7"
+		;;
+	*Fedora*25*)
+		DISTRO="Fedora_25"
+		TOOL=dnf
+		;;
+	*Fedora*26*)
+		DISTRO="Fedora_26"
+		TOOL=dnf
+		;;
+	*openSUSE*Leap*42.2*)
+		DISTRO="openSUSE_Leap_42.2"
+		TOOL=zypper
+		;;
+	*openSUSE*Leap*42.3*)
+		DISTRO="openSUSE_Leap_42.3"
+		TOOL=zypper
+		;;
+	*openSUSE*Tumbleweed*)
+		DISTRO="openSUSE_Tumbleweed"
+		TOOL=zypper
+		;;
+esac
+
+if [[ -z "$DISTRO" ]]
+then
+	echo "No supported RHEL, CentOS, Fedora, or OpenSUSE derivative detected - bailing out..."
+	exit -1
+fi
+echo "Settling for $DISTRO - enabling the Apertium $CADENCE repo..."
+
+URL="http://download.opensuse.org/repositories/home:/TinoDidriksen:/$CADENCE/$DISTRO/home:TinoDidriksen:$CADENCE.repo"
+case $TOOL in
+	yum|dnf)
+		echo "Installing $URL to /etc/yum.repos.d/apertium-$CADENCE.repo..."
+		rm -fv /etc/yum.repos.d/apertium*
+		wget "$URL" -O /etc/yum.repos.d/apertium.repo
+		echo "Running $TOOL updateinfo..."
+		$TOOL updateinfo
+		;;
+	zypper)
+		echo "Installing $URL via zypper..."
+		zypper ar -f "$URL"
+		echo "Running zypper refresh..."
+		zypper refresh
+		;;
+esac
+
+echo "All done - enjoy the packages! If you just want all core tools, do: sudo $TOOL install apertium-all-devel"
