2d5b217a7e971537580761a4be7e107e7959b68d
Tino Didriksen <mail@tinodidriksen.com>
2018-09-17 07:18:12 +0000

    Add -cym, -sma-nob, -smj-nob, -smn-fin

--- apt-get-upgrade.sh
+++ apt-get-upgrade.sh
@@ -6,4 +6,7 @@ apt-get -qfy install cg3
 apt-get -qfy install $(aptitude search '~O apertium' | egrep -o 'apertium-\w+-\w+' | grep -v lex-tools | grep -v all-dev | sort | uniq)
 apt-get -qfy install giella-kal
 # apt-get -qfy install $(aptitude search '~O apertium' | grep 'single language' | egrep -o '(apertium|giella)-\w+' | sort | uniq)
+
+set +e
 su - apertium -c 'screen -r beta-apy -X quit && nice -n20 screen -dmS beta-apy /home/apertium/apertium-apy-beta/run-apy.sh'
+su - katersat -c '~/workbench/inc/sanity-check.php'
