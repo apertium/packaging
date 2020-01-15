fb692acff52d471d2c3356321759c2c80ce49f45
Xavi Ivars <xavi.ivars@gmail.com>
2020-01-15 18:48:43 +0100

    Including eoan into apt repos

--- scripts/apt-install-release.sh
+++ scripts/apt-install-release.sh
@@ -16,7 +16,7 @@ P=`apt-cache policy`
 P="$P "`lsb_release -c`"/dummy"
 P="$P "`grep CODENAME /etc/lsb-release`"/dummy"
 DISTRO="$1"
-for D in sid stretch buster bullseye xenial bionic cosmic disco
+for D in sid stretch buster bullseye xenial bionic cosmic disco eoan
 do
 	if [[ $P == *$D/* ]]
 	then
