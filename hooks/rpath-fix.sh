b06d200bed28cbf5046a5f54cfccfbf7be422936
Tino Didriksen <mail@tinodidriksen.com>
2024-09-12 19:21:03 +0200

    Different macOS rpath hacks

--- hooks/rpath-fix.sh
+++ hooks/rpath-fix.sh
@@ -8,7 +8,11 @@ find . -type f -perm +0100 -or -type f -name '*.dylib' | while read F; do
 	fi
 
 	echo "$F"
-	install_name_tool -change '@rpath/libcg3.1.dylib' '/usr/local/lib/libcg3.1.dylib' "$F" 2>/dev/null
-	install_name_tool -change '@rpath/libfoma.0.dylib' '/usr/local/lib/libfoma.0.dylib' "$F" 2>/dev/null
-	install_name_tool -change '@rpath/liblttoolbox.3.dylib' '/usr/local/lib/liblttoolbox.3.dylib' "$F" 2>/dev/null
+	install_name_tool -add_rpath @executable_path/../lib "$F" 2>/dev/null
+	install_name_tool -add_rpath @loader_path "$F" 2>/dev/null
+	install_name_tool -add_rpath /opt/local/lib "$F" 2>/dev/null
+	install_name_tool -add_rpath /usr/local/lib "$F" 2>/dev/null
+#	install_name_tool -change '@rpath/libcg3.1.dylib' '/usr/local/lib/libcg3.1.dylib' "$F" 2>/dev/null
+#	install_name_tool -change '@rpath/libfoma.0.dylib' '/usr/local/lib/libfoma.0.dylib' "$F" 2>/dev/null
+#	install_name_tool -change '@rpath/liblttoolbox.3.dylib' '/usr/local/lib/liblttoolbox.3.dylib' "$F" 2>/dev/null
 done
