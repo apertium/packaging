1434adbae60aff95364761980f4adfdd0b9f20ce
Tino Didriksen <mail@tinodidriksen.com>
2024-12-13 12:57:44 +0000

    Add --enable-grammarchecker (fixes https://github.com/giellalt/lang-kal/issues/4)

--- mxe-bash.sh
+++ mxe-bash.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -it --rm --name mxe --hostname mxe -e LANG=C.UTF-8 -e LC_ALL=C.UTF-8 -e DEBIAN_FRONTEND=noninteractive -e DEBCONF_NONINTERACTIVE_SEEN=true -e 'DEB_BUILD_OPTIONS=parallel=11' -v $SSH_AUTH_SOCK:/ssh-agent -e SSH_AUTH_SOCK=/ssh-agent -v /opt/mxe:/opt -v /opt/mxe/tmp:/tmp mxe /bin/bash "$@"
+docker run -it --rm --name mxe --hostname mxe -e LANG=C.UTF-8 -e LC_ALL=C.UTF-8 -e DEBIAN_FRONTEND=noninteractive -e DEBCONF_NONINTERACTIVE_SEEN=true -e 'DEB_BUILD_OPTIONS=parallel=11' -v /root/.ssh:/root/.ssh -v $SSH_AUTH_SOCK:/ssh-agent -e SSH_AUTH_SOCK=/ssh-agent -v /opt/mxe:/opt -v /opt/mxe/tmp:/tmp mxe /bin/bash "$@"
